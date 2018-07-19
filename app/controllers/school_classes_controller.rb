cd59fd3c3a5383106d1e9f2fe47477599a5227ca
Meredith Reece <mardebugger@gmail.com>
2018-07-19 13:48:06 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -20,4 +20,16 @@ class SchoolClassesController < ApplicationController
     @school_class.save
     redirect_to school_class_path(@school_class)
   end
+
+  def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(school_class_params(:title))
+	  redirect_to school_class_path(@school_class)
+  end
+
+  private
+
+  def school_class_params(*args)
+    params.require(:school_class).permit(*args)
+  end
 end
